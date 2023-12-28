516ccaeab495f466de974f194c11f0f9f5196311
unknown <renancavalcantercb@protonmail.com>
2023-12-28 04:31:34 -0300

    added text on button

--- ButtonClass.lua
+++ ButtonClass.lua
@@ -1,9 +1,12 @@
 local ButtonClass = {}
 
 function ButtonClass.newButton(options)
-    local button = display.newCircle(options.x, options.y, options.width, options.height)
+    local button = display.newCircle(options.x, options.y, options.width, options.height, options.text)
     button:setFillColor(options.color[1], options.color[2], options.color[3])
     button.value = options.value
+
+    local text = display.newText(options.text, options.x, options.y, native.systemFont, 24)
+    text:setFillColor(0, 0, 0)
 
     function button:tap(event)
         options.tapAction(self.value)
