b21a6a5cb261fa6833c417f1e7a7be9f7830b159
unknown <renancavalcantercb@protonmail.com>
2024-01-02 03:08:20 -0300

    refactor layout and added option button

--- menuScene.lua
+++ menuScene.lua
@@ -4,26 +4,37 @@ local scene = composer.newScene()
 function scene:create(event)
     local sceneGroup = self.view
 
-    local header = display.newText("Money Clicker", display.contentCenterX, 100, native.systemFont, 40)
+    local header = display.newText("Money Clicker", display.contentCenterX, 25, native.systemFont, 40)
     sceneGroup:insert(header)
 
-
-    local startButton = display.newRect(display.contentCenterX, display.contentCenterY, 200, 100)
+    local startButton = display.newRect(display.contentCenterX, 150, 200, 100)
 
     startButton:setFillColor(0, 0, 1)
     sceneGroup:insert(startButton)
 
-    local startButtonText = display.newText("Start", display.contentCenterX, display.contentCenterY, native.systemFont, 40)
+    local startButtonText = display.newText("Start", display.contentCenterX, 150, native.systemFont, 40)
     sceneGroup:insert(startButtonText)
 
-    local function iniciarJogo()
+    local optionsButton = display.newRect(display.contentCenterX, display.contentCenterY + 50, 200, 100)
+    optionsButton:setFillColor(0, 0, 1)
+    sceneGroup:insert(optionsButton)
+
+    local optionsButtonText = display.newText("Options", display.contentCenterX, display.contentCenterY + 50, native.systemFont, 40)
+    sceneGroup:insert(optionsButtonText)
+
+    local footer = display.newText("by: CuTGuArDiAn Studios", display.contentCenterX, display.contentHeight - 75, native.systemFont, 24)
+    sceneGroup:insert(footer)
+
+    local function startGame()
         composer.gotoScene("gameScene")
     end
 
-    local footer = display.newText("by: CuTGuArDiAn Studios", display.contentCenterX, display.contentHeight - 100, native.systemFont, 24)
-    sceneGroup:insert(footer)
+    local function goToOptions()
+        composer.gotoScene("optionsScene")
+    end
 
-    startButton:addEventListener("tap", iniciarJogo)
+    startButton:addEventListener("tap", startGame)
+    optionsButton:addEventListener("tap", goToOptions)
 end
 
 scene:addEventListener("create", scene)
