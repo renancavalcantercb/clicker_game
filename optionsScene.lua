e4799847cdc6a406fe5b78e62fcdb11ce7b4cfef
unknown <renancavalcantercb@protonmail.com>
2024-01-02 03:24:48 -0300

    added back button

--- optionsScene.lua
+++ optionsScene.lua
@@ -7,13 +7,25 @@ function scene:create(event)
     local header = display.newText("Money Clicker", display.contentCenterX, 25, native.systemFont, 40)
     sceneGroup:insert(header)
 
-    -- add a reset save button
-    local resetButton = display.newRect(display.contentCenterX, display.contentCenterY, 200, 100)
+    local resetButton = display.newRect(display.contentCenterX, 150, 200, 100)
     resetButton:setFillColor(0, 0, 1)
     sceneGroup:insert(resetButton)
 
-    local resetButtonText = display.newText("Reset", display.contentCenterX, display.contentCenterY, native.systemFont, 40)
+    local resetButtonText = display.newText("Reset", display.contentCenterX, 150, native.systemFont, 40)
     sceneGroup:insert(resetButtonText)
+
+    local backButton = display.newRect(display.contentCenterX, display.contentCenterY + 50, 200, 100)
+    backButton:setFillColor(0, 0, 1)
+    sceneGroup:insert(backButton)
+
+    local backButtonText = display.newText("Back", display.contentCenterX, display.contentCenterY + 50, native.systemFont, 40)
+    sceneGroup:insert(backButtonText)
+
+    local function goToMenu()
+        composer.gotoScene("menuScene")
+    end
+
+    backButton:addEventListener("tap", goToMenu)
 
     local footer = display.newText("by: CuTGuArDiAn Studios", display.contentCenterX, display.contentHeight - 75, native.systemFont, 24)
     sceneGroup:insert(footer)
